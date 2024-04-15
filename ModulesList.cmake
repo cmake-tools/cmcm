aef4aea1933a0aae9804c12933973afdc33cd50b
flagarde <flagarde.programs@pm.me>
2024-04-15 16:37:20 +0800

    prepare tests

---
+++ ModulesList.cmake
@@ -0,0 +1,8 @@
+#
+# CMakeCM
+# CMake Common Modules.
+#
+# SPDX-FileCopyrightText: 2024 flagarde
+#
+# SPDX-License-Identifier: MIT
+#
---
+++ tests/SetupModulesList.cmake
@@ -0,0 +1,13 @@
+#
+# CMakeCM
+# CMake Common Modules.
+#
+# SPDX-FileCopyrightText: 2024 flagarde
+#
+# SPDX-License-Identifier: MIT
+#
+
+file(DOWNLOAD "https://cmake-tools.github.io/cmmm/latest/GetCMakeMM.cmake" "${CMAKE_CURRENT_BINARY_DIR}/GetCMakeMM.cmake")
+include("${CMAKE_CURRENT_BINARY_DIR}/GetCMakeMM.cmake")
+cmmm(VERSION latest)
+cmmm_modules_list(URL "gh:cmake-tools/cmcm")
